b61ca24fcbbafc27648f5743e2c9e251300f83fc
Marina <maryna.seidel@yahoo.de>
2025-08-01 16:41:34 +0200

    search functionality added

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import styles from "./SearchField.module.css";
 
-const SearchField = () => {
-  
+const SearchField = ({ onSearch }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter" && inputValue.trim() !== "") {
+      onSearch(inputValue); // Forward value to parent
+      setInputValue("");
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
       <input
+        id="search"
+        name="search"
         placeholder="Filter dishes..."
-        type="text"     
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
     </div>
   );
